7edb952662dbf404d44de172d2e94b10bf502728
Federigi Pablo <pfederigi@gmail.com>
2013-09-24 20:19:04 -0300

    Un commit en una branch ??

--- test/gittest/src/gittest/Main.java
+++ test/gittest/src/gittest/Main.java
@@ -2,9 +2,10 @@ package gittest;
 
 public final class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Esta es una prueba 6");
+    public static void main(String[] args) {
+        //Esta línea fue agregada de netbeans en el first_branch
+        System.out.println("Esta es una prueba 6");
 
-	}
-
+    }
+    
 }
