aa6aebb1c5c0991fde8e9e9205b82da18044399e
Federigi Pablo <pfederigi@gmail.com>
2013-09-24 19:37:52 -0300

    second commit

--- test/gittest/src/gittest/Main.java
+++ test/gittest/src/gittest/Main.java
@@ -3,7 +3,7 @@ package gittest;
 public final class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Esta es una prueba");
+		System.out.println("Esta es una prueba 2");
 
 	}
 
